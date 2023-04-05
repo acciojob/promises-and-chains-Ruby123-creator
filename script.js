c59b3095f2685104101e9ac3435f580fdd2da502
Ruby pal <rubyp20001@gmail.com>
2023-04-05 10:08:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-//your JS code here. If required.
-const form = document.querySelector('#myForm');
-  const nameInput = document.querySelector('#name');
-  const ageInput = document.querySelector('#age');
-  const btn = document.querySelector('#btn');
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent form from submitting normally
-
-    if (nameInput.value === '' || ageInput.value === '') {
-      alert('Please fill out all fields.'); // Show validation error
-      return;
+let inputs = document.querySelectorAll("input");
+inputs[2].addEventListener("click",check)
+let obj ={};
+function check(e){
+    e.preventDefault()
+    if(inputs[0].value!==""&&inputs[1].value!==""){
+            checkPromise().then((resolve)=>{alert(resolve)})
+            .catch((reason)=>{
+                alert(reason)
+            })
     }
+    else{
+        document.querySelector("span").innerText = "please fill the inputs";
+    }
+    obj.name = inputs[0].value;
+    obj.age = inputs[1].value;
+}
 
-    const age = parseInt(ageInput.value);
-
-    // Check if age is above 18 after 4 seconds
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (age >= 18) {
-          resolve();
-        } else {
-          reject();
-        }
-      }, 4000);
-    });
-
-    promise
-      .then(() => {
-        // Show welcome message if age is above 18
-        alert(`Welcome, ${nameInput.value}. You can vote.`);
-      })
-      .catch(() => {
-        // Show rejection message if age is below 18
-        alert(`Oh sorry ${nameInput.value}. You aren't old enough.`);
-      });
-  });
+ function checkPromise(){
+    
+    return new Promise((resolve ,reject)=>{
+        // console.log(inputs[0].value)
+        setTimeout(()=>{
+            // console.log(obj);
+            if(obj.age>18){
+                resolve(`Welcome, ${obj.name} . You can vote.`)
+                
+            }
+            else{
+                reject(`Oh sorry ${obj.name}. You aren't old enough.`)
+                
+            }
+        },4000)
+    })
+    
+}
